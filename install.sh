dc33fe1331f2f7258aa3a3a88a6588316592779e
Artem Usenko <arata.em@gmail.com>
2024-01-05 09:55:21 +0200

    avoid prompt

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo apt install software-properties-common
+sudo apt install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible
 ansible-playbook -K playbooks/local.yml --diff
